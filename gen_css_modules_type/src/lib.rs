68362b78b51ed04dc9340f89e555babd1bb01e79
klcantrell <cantrellkalalau@gmail.com>
2020-01-11 22:01:13 -0500

    gen_css_modules_type: get contents from css file

--- gen_css_modules_type/src/lib.rs
+++ gen_css_modules_type/src/lib.rs
@@ -1,6 +1,7 @@
 use notify::{EventFn, RecommendedWatcher, RecursiveMode, Result as NotifyResult, Watcher};
 use regex::Regex;
 use std::fs::File;
+use std::io::prelude::Read;
 use std::io::{self, BufRead, Error};
 use std::path::PathBuf;
 
@@ -17,7 +18,7 @@ pub fn handle_css_change(content: &str) -> String {
   let rules = parse_rules(content);
   let rules = remove_dot(rules);
   let type_defs = type_defs_of_rules(rules);
-  let type_defs = create_type_def_file(type_defs);
+  let type_defs = create_type_defs_file(type_defs);
   type_defs
 }
 
@@ -40,7 +41,7 @@ fn type_defs_of_rules(rule_names: Vec<&str>) -> Vec<String> {
     .collect()
 }
 
-fn create_type_def_file(type_defs: Vec<String>) -> String {
+fn create_type_defs_file(type_defs: Vec<String>) -> String {
   type_defs
     .into_iter()
     .fold(String::from(""), |content, type_def| {
@@ -55,20 +56,52 @@ pub fn create_watcher(path: &str, handler: Box<dyn EventFn>) -> NotifyResult<Rec
 }
 
 pub fn read_line(line: &mut String) -> Result<(), Error> {
+  line.clear();
   io::stdin().lock().read_line(line)?;
   Ok(())
 }
 
 pub fn handle_on_modify(paths: Vec<PathBuf>) -> () {
-  paths.into_iter().for_each(|path| match path.extension() {
+  let path = &paths[0];
+  match path.extension() {
     Some(os_ext) => {
       if let Some("css") = os_ext.to_str() {
-        println!("event: {:?}", path);
-        // let f = File::open(path);
-        let type_defs = handle_css_change(TEST_CSS);
-        println!("{}", type_defs);
+        let type_defs_filename = create_type_defs_filename(path);
+        let contents = extract_file_contents(path);
+        let type_defs = handle_css_change(&contents);
+        println!("type defs filename {}", type_defs_filename);
+        println!("type defs {}", type_defs);
       }
     }
     _ => (),
-  });
+  }
+}
+
+fn extract_file_contents(path: &PathBuf) -> String {
+  let mut retries = 0;
+  let mut content = String::new();
+  while retries < 10_000 {
+    let file = File::open(path);
+    if let Ok(mut file_contents) = file {
+      if let Ok(_) = file_contents.read_to_string(&mut content) {
+        break;
+      } else {
+        retries = retries + 1;
+      }
+    } else {
+      retries = retries + 1;
+    }
+  }
+  content
+}
+
+fn create_type_defs_filename(path: &PathBuf) -> String {
+  let unknown_name = "unknown";
+  match path.file_name() {
+    Some(name) => match name.to_str() {
+      Some(name) => format!("{}.d.ts", name),
+      None => format!("{}.d.ts", unknown_name),
+    },
+    None => format!("{}.d.ts", unknown_name),
+  }
 }
