e74df4229d7ec932df59d6842ab109899cfb54b2
klcantrell <cantrellkalalau@gmail.com>
2020-01-12 12:33:47 -0500

    gen_css_modules_type: add saving of type defs file

--- gen_css_modules_type/src/lib.rs
+++ gen_css_modules_type/src/lib.rs
@@ -2,19 +2,44 @@ use notify::{EventFn, RecommendedWatcher, RecursiveMode, Result as NotifyResult,
 use regex::Regex;
 use std::fs::File;
 use std::io::prelude::Read;
-use std::io::{self, BufRead, Error};
+use std::io::{self, BufRead, Error, Write};
 use std::path::PathBuf;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub const PATH_TO_WATCH: &str = ".";
 
-pub const TEST_CSS: &str = "
-    .sup { \
-    } \
-    .dude { \
-    } \
-";
+pub fn create_watcher(path: &str, handler: Box<dyn EventFn>) -> NotifyResult<RecommendedWatcher> {
+  let mut watcher: RecommendedWatcher = Watcher::new_immediate(handler)?;
+  watcher.watch(path, RecursiveMode::Recursive)?;
+  Ok(watcher)
+}
 
-pub fn handle_css_change(content: &str) -> String {
+pub fn read_line(line: &mut String) -> Result<(), Error> {
+  line.clear();
+  io::stdin().lock().read_line(line)?;
+  Ok(())
+}
+
+pub fn handle_on_modify(paths: Vec<PathBuf>) -> () {
+  if !paths.is_empty() {
+    let path = &paths[0];
+    match path.extension() {
+      Some(os_ext) => {
+        if let Some("css") = os_ext.to_str() {
+          let type_defs_filename = create_type_defs_filename(path);
+          let contents = extract_file_contents(path);
+          let type_defs = handle_css_change(&contents);
+          save_type_defs(type_defs, type_defs_filename);
+        }
+      }
+      _ => (),
+    }
+  } else {
+    println!("Unknown path, did nothing");
+  }
+}
+
+fn handle_css_change(content: &str) -> String {
   let rules = parse_rules(content);
   let rules = remove_dot(rules);
   let type_defs = type_defs_of_rules(rules);
@@ -31,13 +56,20 @@ fn parse_rules(css: &str) -> Vec<&str> {
 }
 
 fn remove_dot(rules: Vec<&str>) -> Vec<&str> {
-  rules.into_iter().map(|rule| &rule[1..]).collect()
+  rules
+    .into_iter()
+    .map(|rule| {
+      let mut graphemes_iter = rule.graphemes(true);
+      graphemes_iter.next();
+      graphemes_iter.as_str()
+    })
+    .collect()
 }
 
 fn type_defs_of_rules(rule_names: Vec<&str>) -> Vec<String> {
   rule_names
     .into_iter()
-    .map(|rule_name| format!("export const {}: string", rule_name))
+    .map(|rule_name| format!("export const {}: string;", rule_name))
     .collect()
 }
 
@@ -45,42 +77,14 @@ fn create_type_defs_file(type_defs: Vec<String>) -> String {
   type_defs
     .into_iter()
     .fold(String::from(""), |content, type_def| {
-      format!("{} {} \n", content, type_def)
+      format!("{}{}\n", content, type_def)
     })
-}
-
-pub fn create_watcher(path: &str, handler: Box<dyn EventFn>) -> NotifyResult<RecommendedWatcher> {
-  let mut watcher: RecommendedWatcher = Watcher::new_immediate(handler)?;
-  watcher.watch(path, RecursiveMode::Recursive)?;
-  Ok(watcher)
-}
-
-pub fn read_line(line: &mut String) -> Result<(), Error> {
-  line.clear();
-  io::stdin().lock().read_line(line)?;
-  Ok(())
-}
-
-pub fn handle_on_modify(paths: Vec<PathBuf>) -> () {
-  let path = &paths[0];
-  match path.extension() {
-    Some(os_ext) => {
-      if let Some("css") = os_ext.to_str() {
-        let type_defs_filename = create_type_defs_filename(path);
-        let contents = extract_file_contents(path);
-        let type_defs = handle_css_change(&contents);
-        println!("type defs filename {}", type_defs_filename);
-        println!("type defs {}", type_defs);
-      }
-    }
-    _ => (),
-  }
 }
 
 fn extract_file_contents(path: &PathBuf) -> String {
   let mut retries = 0;
   let mut content = String::new();
-  while retries < 10_000 {
+  while retries < 1_000 {
     let file = File::open(path);
     if let Ok(mut file_contents) = file {
       if let Ok(_) = file_contents.read_to_string(&mut content) {
@@ -104,4 +108,22 @@ fn create_type_defs_filename(path: &PathBuf) -> String {
     },
     None => format!("{}.d.ts", unknown_name),
   }
+}
+
+fn save_type_defs(content: String, filename: String) -> () {
+  let mut retries = 0;
+  while retries < 1_000 {
+    let file = File::create(&filename);
+    if let Ok(mut opened_file) = file {
+      if let Ok(_) = opened_file.write_all(content.as_bytes()) {
+        println!("Saved type def to {}", filename);
+        return ();
+      } else {
+        retries = retries + 1;
+      }
+    } else {
+      retries = retries + 1;
+    }
+  }
+  println!("Failed to save type def to {}", filename);
 }
