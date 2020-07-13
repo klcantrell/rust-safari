66cd474008be60f8e8876d206d810e0b95d83f3f
klcantrell <cantrellkalalau@gmail.com>
2020-07-12 20:34:22 -0400

    compose-in-rust: initial commit

---
+++ compose_in_rust/src/main.rs
@@ -0,0 +1,56 @@
+fn main() {
+    let append_exclamation = |word: &str| -> String {
+        let mut new_word = String::from(word);
+        new_word.push('!');
+        return new_word;
+    };
+    let num = |word: String| -> usize { word.len() };
+    let add_one = |num: usize| -> usize { num + 1 };
+
+    let composed = pipe().from(append_exclamation).to(num).to(add_one);
+
+    println!("{}", composed.apply("Hi")); // prints 4
+}
+
+fn pipe() -> PipeMaker {
+    PipeMaker::new()
+}
+
+struct PipeMaker {}
+
+impl PipeMaker {
+    fn new() -> PipeMaker {
+        PipeMaker {}
+    }
+
+    fn from<'a, F, T, U>(self, wrapped_function: F) -> Pipe<'a, T, U>
+    where
+        F: 'a + Fn(T) -> U,
+    {
+        Pipe {
+            wrapped_function: Box::new(wrapped_function),
+        }
+    }
+}
+
+struct Pipe<'a, T, U> {
+    wrapped_function: Box<dyn Fn(T) -> U + 'a>,
+}
+
+impl<'a, T, U> Pipe<'a, T, U> {
+    fn to<F, V>(self, input: F) -> Pipe<'a, T, V>
+    where
+        F: 'a + Fn(U) -> V,
+        T: 'a,
+        U: 'a,
+        V: 'a,
+    {
+        Pipe {
+            wrapped_function: Box::new(move |arg: T| -> V { input((self.wrapped_function)(arg)) }),
+        }
+    }
+
+    fn apply(self, arg: T) -> U {
+        (self.wrapped_function)(arg)
+    }
+}
