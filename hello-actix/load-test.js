1fc7c613f056eadc564d23945af7c0d5ce8df5ed
klcantrell <cantrellkalalau@gmail.com>
2023-06-05 21:55:19 -0400

    [hello_actix]: implement weather forecast test API. include load test.

---
+++ hello-actix/load-test.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 200,
+  duration: '1m',
+};
+
+export default function() {
+  http.get('http://hello-actix.eastus.cloudapp.azure.com/api/hello-actix');
+}
+
--- hello-actix/src/main.rs
+++ hello-actix/src/main.rs
@@ -1,24 +1,55 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use rand::{thread_rng, Rng};
 use serde::Serialize;
 use std::net::Ipv4Addr;
+use time::{Duration, OffsetDateTime};
 
 #[derive(Serialize)]
-struct HelloResponse {
-    greeting: String,
+struct WeatherForecast {
+    #[serde(with = "time::serde::iso8601")]
+    date: OffsetDateTime,
+
+    #[serde(rename = "name")]
+    temperature_c: i32,
+
+    summary: String,
 }
 
-#[get("/api/hello-actix")]
+const SUMMARIES: [&str; 10] = [
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+];
+
+#[get("/weatherforecast")]
 async fn hello_actix() -> impl Responder {
-    HttpResponse::Ok().json(HelloResponse { greeting: "Hello from Actix on a VM!".to_string() })
+    HttpResponse::Ok().json(
+        (1..5)
+            .map(|index| WeatherForecast {
+                date: OffsetDateTime::now_utc() + Duration::days(index),
+                temperature_c: 32,
+                summary: SUMMARIES[make_random(0, SUMMARIES.len() - 1)].to_string(),
+            })
+            .collect::<Vec<WeatherForecast>>(),
+    )
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello_actix)
-    })
-    .bind((Ipv4Addr::LOCALHOST, 3000))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello_actix))
+        .bind((Ipv4Addr::LOCALHOST, 3000))?
+        .run()
+        .await
+}
+
+fn make_random(start: usize, end_inclusive: usize) -> usize {
+    let mut rng = thread_rng();
+    rng.gen_range(start..=end_inclusive)
 }
