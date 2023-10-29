d1dc5ba08a7de2eb02f1c891d9b54c53bc776f71
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-10-28 22:23:10 -0400

    Start [Rust Adventure]/[2048 with Bevy]

---
+++ boxes/src/main.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+
+mod colors;
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::hex("#1f2638").unwrap()))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "2048".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_systems((setup, spawn_board))
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+const TILE_SIZE: f32 = 40.0;
+
+fn spawn_board(mut commands: Commands) {
+    let board = Board { size: 4 };
+    let physical_board_size = f32::from(board.size) * TILE_SIZE;
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: colors::BOARD,
+                custom_size: Some(Vec2::new(physical_board_size, physical_board_size)),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(board);
+}
