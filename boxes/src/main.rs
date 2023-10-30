edb54eb244f5bb5eb80a633ba9e0f9ee6163984c
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-10-29 21:07:13 -0400

    [Rust Adventure]/[2048 with Bevy] Add tiles with gutters to the board

--- boxes/src/main.rs
+++ boxes/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use itertools::Itertools;
 
 mod colors;
 
@@ -26,10 +27,12 @@ fn setup(mut commands: Commands) {
 }
 
 const TILE_SIZE: f32 = 40.0;
+const TILE_SPACER: f32 = 10.0;
 
 fn spawn_board(mut commands: Commands) {
     let board = Board { size: 4 };
-    let physical_board_size = f32::from(board.size) * TILE_SIZE;
+    let physical_board_size =
+        f32::from(board.size) * TILE_SIZE + f32::from(board.size + 1) * TILE_SPACER;
 
     commands
         .spawn(SpriteBundle {
@@ -39,6 +42,29 @@ fn spawn_board(mut commands: Commands) {
                 ..default()
             },
             ..default()
+        })
+        .with_children(|builder| {
+            let offset = -physical_board_size / 2.0 + TILE_SIZE / 2.0;
+
+            for tile in (0..board.size).cartesian_product(0..board.size) {
+                builder.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: colors::TILE_PLACEHOLDER,
+                        custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                        ..default()
+                    },
+                    transform: Transform::from_xyz(
+                        offset
+                            + f32::from(tile.0) * TILE_SIZE
+                            + f32::from(tile.0 + 1) * TILE_SPACER,
+                        offset
+                            + f32::from(tile.1) * TILE_SIZE
+                            + f32::from(tile.1 + 1) * TILE_SPACER,
+                        1.0,
+                    ),
+                    ..default()
+                });
+            }
         })
         .insert(board);
 }
